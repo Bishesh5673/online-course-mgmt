da4c1f9ad7ad1f545242fce9357a510cb9ff307a
Bishesh5673 <bisesmhrzn101@gmail.com>
2026-02-17 20:48:25 +0545

    home page start

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
-    <div>
-      home
-    </div>
-  )
+    <main>
+      <div className="bg-green-400 p-10">
+        <div className="w-[500px] space-y-8">
+          <h1 className="text-white text-6xl font-bold">Career-Ready IT Training in Nepal</h1>
+          <p className="text-white text-2xl">
+            Gain job-ready skills with our IT courses in Nepal. Elevate your
+            career prospects today!
+          </p>
+          <button className="bg-black text-white h-15 w-40 text-2xl px-1 ">
+            Enroll Now
+          </button>
+        </div>
+      </div>
+    </main>
+  );
 }
 
-export default Home
+export default Home;
